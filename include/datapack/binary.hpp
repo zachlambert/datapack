14613c5a626472781d43c1ee80a9f95e14b2b7c0
Zach Lambert <zachalambert@gmail.com>
2024-05-16 21:13:48 +0100

    Finished binary writer and reader

--- include/datapack/binary.hpp
+++ include/datapack/binary.hpp
@@ -9,53 +9,112 @@
 
 namespace datapack {
 
-class BinaryReader: public Reader {
+class BinaryWriter : public Writer {
+public:
+    BinaryWriter(std::vector<std::uint8_t>& data):
+        data(data)
+    {}
+
+    void value_i32(std::int32_t value) override { value_number(value); }
+    void value_i64(std::int64_t value) override { value_number(value); }
+    void value_u32(std::uint32_t value) override { value_number(value); }
+    void value_u64(std::uint64_t value) override { value_number(value); }
+
+    void value_f32(float value) override { value_number(value); }
+    void value_f64(double value) override { value_number(value); }
+
+    void value_string(const std::string&) override;
+    void value_bool(bool value) override;
+
+    void enumerate(int value, const std::vector<const char*>& labels) override;
+    void optional(bool has_value) override;
+    void variant(const char* label, const std::vector<const char*>& labels) override;
+
+    void binary(std::size_t size, const std::uint8_t* data) override;
+
+    void object_begin() override {}
+    void object_end() override {}
+    void object_next(const char* key) override {}
+
+    void tuple_begin() override {}
+    void tuple_end() override {}
+    void tuple_next() override {}
+
+    void map_begin() override;
+    void map_end() override;
+    void map_next(const std::string& key) override;
+
+    void list_begin() override;
+    void list_end() override;
+    void list_next() override;
+
+private:
+    template <typename T>
+    void value_number(T value) {
+        std::size_t pos = data.size();
+        data.resize(data.size() + sizeof(T));
+        *((T*)&data[pos]) = value;
+    }
+
+    std::vector<std::uint8_t>& data;
+};
+
+
+class BinaryReader : public Reader {
 public:
     BinaryReader(const std::vector<std::uint8_t>& data):
         data(data),
         pos(0)
     {}
 
-    void value_f64(double& value) override;
-    void value_i32(int& value) override;
+    void value_i32(std::int32_t& value) override { value_number(value); }
+    void value_i64(std::int64_t& value) override { value_number(value); }
+    void value_u32(std::uint32_t& value) override { value_number(value); }
+    void value_u64(std::uint64_t& value) override { value_number(value); }
 
-    bool optional_begin() override;
-    void optional_end() override {}
+    void value_f32(float& value) override { value_number(value); }
+    void value_f64(double& value) override { value_number(value); }
 
-    void variant_begin(const std::vector<std::string>& type) override {}
-    bool variant_match(const char* type) override;
-    void variant_end() override {}
+    void value_string(std::string& value) override;
+    void value_bool(bool& value) override;
+
+    int enumerate(const std::vector<const char*>& labels) override;
+    bool optional() override;
+    const char* variant(const std::vector<const char*>& labels) override;
+
+    std::size_t binary_size(std::size_t expected_size=0) override;
+    void binary_data(std::uint8_t* data) override;
 
     void object_begin() override {}
     void object_end() override {}
     void object_next(const char* key) override {}
 
+    void tuple_begin() override {}
+    void tuple_end() override {}
+    void tuple_next() override {}
+
+    void map_begin() override;
+    void map_end() override;
+    bool map_next(std::string& key) override;
+
+    void list_begin() override;
+    void list_end() override;
+    bool list_next() override;
+
 private:
+    template <typename T>
+    void value_number(T value) {
+        if (pos + sizeof(T) > data.size()) {
+            error("Input data is too short");
+            return;
+        }
+        value = *((T*)&data[pos]);
+        pos += sizeof(T);
+    }
+
     const std::vector<std::uint8_t>& data;
     std::size_t pos;
-};
-
-class BinaryWriter: public Writer {
-public:
-    BinaryWriter(std::vector<std::uint8_t>& data):
-        data(data)
-    {}
-
-    void value_f64(const double& value) override;
-    void value_i32(const int& value) override;
-
-    void optional_begin(bool has_value) override;
-    void optional_end() override {}
-    void variant_begin() override {}
-    void variant_match(const char* type) override;
-    void variant_end() override {}
-
-    void object_begin() override {}
-    void object_end() override {}
-    void object_next(const char* key) override {}
-
-private:
-    std::vector<std::uint8_t>& data;
+    std::size_t next_binary_size;
 };
 
 } // namespace datapack
--- src/binary.cpp
+++ src/binary.cpp
@@ -3,71 +3,167 @@
 
 namespace datapack {
 
-void BinaryReader::value_f64(double& value) {
-    if (pos + sizeof(double) > data.size()) {
-        error("Input data is too short");
-        return;
-    }
-    value = *((double*)&data[pos]);
-    pos += sizeof(double);
+void BinaryWriter::value_string(const std::string& value) {
+    std::size_t size = value.size() + 1;
+    std::size_t pos = data.size();
+    data.resize(data.size() + size);
+    strncpy((char*)&data[pos], value.c_str(), size);
 }
 
-void BinaryReader::value_i32(int& value) {
-    if (pos + sizeof(int) > data.size()) {
-        error("Input data is too short");
-        return;
-    }
-    value = *((int*)&data[pos]);
-    pos += sizeof(int);
+void BinaryWriter::value_bool(bool value) {
+    data.push_back(value ? 0x01 : 0x00);
 }
 
-bool BinaryReader::optional_begin() {
+
+void BinaryWriter::enumerate(int value, const std::vector<const char*>& labels) {
+    value_number(value);
+}
+
+void BinaryWriter::optional(bool has_value) {
+    value_bool(has_value);
+}
+
+void BinaryWriter::variant(const char* label, const std::vector<const char*>& labels) {
+    value_string(label);
+}
+
+
+void BinaryWriter::binary(std::size_t binary_size, const std::uint8_t* binary_data) {
+    std::size_t pos = data.size();
+    data.resize(pos + binary_size);
+    std::memcpy(&data[pos], binary_data, binary_size);
+}
+
+void BinaryWriter::map_begin() {
+
+}
+
+void BinaryWriter::map_end() {
+    value_bool(false);
+}
+
+void BinaryWriter::map_next(const std::string& key) {
+    value_bool(true);
+    value_string(key);
+}
+
+
+void BinaryWriter::list_begin() {
+
+}
+
+void BinaryWriter::list_end() {
+    value_bool(false);
+}
+
+void BinaryWriter::list_next() {
+    value_bool(true);
+}
+
+
+void BinaryReader::value_string(std::string& value) {
+    std::size_t max_len = data.size() - pos;
+    std::size_t len = strnlen((char*)&data[pos], max_len);
+    if (len == max_len) {
+        error("Unterminated string");
+    }
+    value = (char*)&data[pos];
+    pos += (len + 1);
+}
+
+void BinaryReader::value_bool(bool& value) {
     if (pos + 1 > data.size()) {
+        error("Input data too short");
+    }
+    std::uint8_t byte = data[pos];
+    if (byte >= 0x02) {
+        error("Unexpected byte for bool");
+    }
+    value = (byte == 0x01);
+}
+
+
+int BinaryReader::enumerate(const std::vector<const char*>& labels) {
+    int value;
+    value_number(value);
+    return value;
+}
+
+bool BinaryReader::optional() {
+    bool has_value;
+    value_bool(has_value);
+    return has_value;
+}
+
+const char* BinaryReader::variant(const std::vector<const char*>& labels) {
+    std::size_t max_len = data.size() - pos;
+    std::size_t len = strnlen((char*)&data[pos], max_len);
+    if (len == max_len) {
+        error("Unterminated string");
+    }
+    for (const auto& label: labels) {
+        if (std::strncmp(label, (char*)&data[pos], max_len) == 0) {
+            pos += (len + 1);
+            return label;
+        }
+    }
+    error("Unknown variant type");
+    return "";
+}
+
+
+std::size_t BinaryReader::binary_size(std::size_t expected_size) {
+    std::uint64_t size;
+    value_number(size);
+    if (expected_size != 0 && size != expected_size) {
+        error("Binary size doesn't match expected value");
+    }
+    next_binary_size = size;
+    return size;
+}
+
+void BinaryReader::binary_data(std::uint8_t* output_data) {
+    std::size_t size = next_binary_size;
+    if (pos + size > data.size()) {
         error("Input data is too short");
     }
-    std::uint8_t flag = data[pos];
-    pos++;
-    if (flag == 0x00) {
-        return false;
-    } else if (flag == 0x01) {
-        return true;
-    } else {
-        error("Unexpected byte value for optional flag");
+    std::memcpy(output_data, &data[pos], size);
+    pos += size;
+}
+
+
+void BinaryReader::map_begin() {
+    // Do nothing
+}
+
+void BinaryReader::map_end() {
+    // Do nothing
+}
+
+bool BinaryReader::map_next(std::string& key) {
+    bool has_next;
+    value_bool(has_next);
+    if (has_next) {
         return false;
     }
+    value_string(key);
+    return true;
 }
 
-bool BinaryReader::variant_match(const char* label) {
-    bool match = std::strncmp(label, (char*)&data[pos], data.size()-pos) == 0;
-    if (match) {
-        pos += (std::strlen(label) + 1);
-        return true;
-    } else {
-        return false;
-    }
+
+void BinaryReader::list_begin() {
+    // Do nothing
 }
 
-void BinaryWriter::value_f64(const double& value) {
-    std::size_t pos = data.size();
-    data.resize(data.size() + sizeof(double));
-    *((double*)&data[pos]) = value;
+void BinaryReader::list_end() {
+    // Do nothing
 }
 
-void BinaryWriter::value_i32(const int& value) {
-    std::size_t pos = data.size();
-    data.resize(data.size() + sizeof(int));
-    *((int*)&data[pos]) = value;
+bool BinaryReader::list_next() {
+    bool has_next;
+    value_bool(has_next);
+    return has_next;
 }
 
-void BinaryWriter::optional_begin(bool has_value) {
-    data.push_back(has_value ? 0x01 : 0x00);
-}
-
-void BinaryWriter::variant_match(const char* label) {
-    std::size_t length = strlen(label) + 1;
-    std::size_t pos = data.size();
-    data.resize(pos + length);
-    strncpy((char*)&data[pos], label, data.size() - pos);
-}
 
 } // namespace datapack

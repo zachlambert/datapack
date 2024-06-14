9e9251ec659533b6ddaafd499ff130a3731036f6
Zach Lambert <zachalambert@gmail.com>
2024-06-14 15:05:05 +0100

    Removed demos that have been converted to tests

--- demo/util/debug.cpp
+++
@@ -1,8 +0,0 @@
-#include <datapack/examples/entity.hpp>
-#include <datapack/util/debug.hpp>
-
-int main() {
-    Entity value = Entity::example();
-    std::cout << datapack::debug(value) << std::endl;
-    return 0;
-}

85f3cd3d0b6f1b9f2fafaa79c0afb400e0d9c9bd
Zach Lambert <zachalambert@gmail.com>
2026-01-28 21:26:28 +0000

    Added file reader and writer

---
+++ src/file.cpp
@@ -0,0 +1,82 @@
+#include "datapack/file.hpp"
+#include <cstring>
+
+namespace datapack {
+
+static const char* SPECIAL = "DATAPACK";
+
+FileWriter::FileWriter(std::ofstream& os) : os(os) {
+  os << SPECIAL;
+}
+
+void FileWriter::write_chunk(
+    const std::string& label,
+    std::uint64_t hash,
+    const std::vector<std::uint8_t>& data) {
+  // Write label
+  std::uint32_t label_size = label.size();
+  os.write((const char*)&label_size, sizeof(label_size));
+  os.write(label.data(), label.size());
+
+  // Write hash
+  os.write((const char*)&hash, sizeof(hash));
+
+  // Write data
+  std::uint64_t data_size = data.size();
+  os.write((const char*)&data_size, sizeof(data_size));
+  os.write((const char*)data.data(), data.size());
+}
+
+FileReader::FileReader(std::ifstream& is) : is(is) {
+  std::string special_buff = SPECIAL;
+  is.read(special_buff.data(), 8);
+  if (special_buff != SPECIAL) {
+    throw FileError();
+  }
+}
+
+std::optional<std::string> FileReader::next() {
+  if (is.eof()) {
+    return std::nullopt;
+  }
+  std::uint32_t label_size;
+  if (!is.read((char*)&label_size, sizeof(label_size))) {
+    throw FileError();
+  }
+  current_label.resize(label_size);
+  if (!is.read(current_label.data(), current_label.size())) {
+    throw FileError();
+  }
+  return current_label;
+}
+
+std::tuple<std::uint64_t, std::vector<std::uint8_t>> FileReader::read_chunk_remainder() {
+  std::uint64_t hash;
+  if (!is.read((char*)&hash, sizeof(hash))) {
+    throw FileError();
+  }
+  std::uint64_t data_size;
+  if (!is.read((char*)&data_size, sizeof(data_size))) {
+    throw FileError();
+  }
+  std::vector<std::uint8_t> data(data_size);
+  if (!is.read((char*)data.data(), data.size())) {
+    throw FileError();
+  }
+  return std::make_tuple(hash, data);
+}
+
+void FileReader::skip() {
+  if (!is.seekg(sizeof(std::uint64_t), std::ios::cur)) {
+    throw FileError();
+  }
+  std::uint64_t data_size;
+  if (!is.read((char*)&data_size, sizeof(data_size))) {
+    throw FileError();
+  }
+  if (!is.seekg(data_size, std::ios::cur)) {
+    throw FileError();
+  }
+}
+
+} // namespace datapack
